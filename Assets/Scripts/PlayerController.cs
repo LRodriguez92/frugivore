0213afc007683eabfaa3882aad2a5a5dd05d0243
Leo <leonardo.rodriguez92@gmail.com>
2021-03-19 12:23:45 -0400

    changed to iso build

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     private Mover mover;
     private SpriteRenderer spriteRenderer;
 
+    public float movementSpeed = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,19 +21,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Moving Left
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
+
+        if (!Mathf.Approximately(0, movement))
         {
-            mover.AccelerateInDirection(new Vector2(-1, 0));
-            spriteRenderer.flipX = true;
+            transform.rotation = movement > 0 ? Quaternion.Euler(0, -180, 0) : Quaternion.identity;
         }
 
-        //Moving Right
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
-            mover.AccelerateInDirection(new Vector2(1, 0));
-            spriteRenderer.flipX = false;
-        }
+        ////Moving Left
+        //if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        //{
+        //    mover.AccelerateInDirection(new Vector2(-1, 0));
+        //    spriteRenderer.flipX = true;
+        //}
+
+        ////Moving Right
+        //if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        //{
+        //    mover.AccelerateInDirection(new Vector2(1, 0));
+        //    spriteRenderer.flipX = false;
+        //}
 
     }
 }
